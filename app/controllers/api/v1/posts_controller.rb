7a516ec2ecd5322771bc052c94af8dc7bd01653a
Lev Brie <lev.brie@gmail.com>
2013-07-31 22:20:28 -0400

    angular install almost complete

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -10,6 +10,18 @@ module Api
 			def create
 				respond_with Post.create(params[:post])
 			end
+
+      def show
+        respond_with Post.find(params[:id])
+      end
+
+      def update 
+        respond_with Post.update(params[:id], params[:post])
+      end
+
+      def destroy
+        respond_with Post.destroy(params[:id])
+      end
 		end
 	end
 end
